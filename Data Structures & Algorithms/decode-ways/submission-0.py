794fbd0c2a046b371b56f75c1c8b845031a356db
Saad Ahmad <67042577+saadahmedsh@users.noreply.github.com>
2026-05-02 12:18:56 +0200

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDecodings(self, decode_str: str) -> int:
+        if not decode_str or decode_str[0] == "0":
+            return 0
+        
+        n = len(decode_str)
+        dp = [0] * (n + 1)
+        
+        
+        dp[0] = 1 
+        dp[1] = 1 
+        
+        for i in range(2, n + 1):
+            if decode_str[i - 1] != "0":
+                dp[i] += dp[i - 1]
+                
+    
+            two_digit = int(decode_str[i-2 : i])
+            if 10 <= two_digit <= 26:
+                dp[i] += dp[i - 2]
+                
+        return dp[n]
+            
